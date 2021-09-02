0b2ab27a71dbe405e25e3c3b05b20251270717ec
iProsto <lenovo.arxi@gmail.com>
2021-09-02 16:50:19 +0300

    add vertical persons banner;

--- lib/pages/contacts/contacts_page.dart
+++ lib/pages/contacts/contacts_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pr_flutter_2_phonebook/pages/base_page_widget.dart';
+import 'package:pr_flutter_2_phonebook/res/app_colors.dart';
 import 'package:pr_flutter_2_phonebook/widgets/app_bar/contacts_app_bar_widget.dart';
+import 'package:pr_flutter_2_phonebook/widgets/person/vertical_banner/person_v_widget.dart';
 import 'package:pr_flutter_2_phonebook/widgets/search_field/search_field_widget.dart';
 
 class ContactsPage extends StatelessWidget {
@@ -13,6 +15,35 @@ class ContactsPage extends StatelessWidget {
         Padding(
           padding: EdgeInsets.only(left: 16, right: 16, top: 6, bottom: 26),
           child: SearchFieldWidget(),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              PersonVWidget(),
+              PersonVWidget(),
+              PersonVWidget(),
+              PersonVWidget(),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 17),
+          padding: EdgeInsets.only(left: 17),
+          width: MediaQuery.of(context).size.width,
+          height: 26,
+          color: AppColors.lightBackground,
+          child: Row(
+              children: [
+            Text(
+              'A',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+            ),
+          ]),
         ),
         ElevatedButton(
           onPressed: () {
---
+++ lib/widgets/person/vertical_banner/person_v_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pr_flutter_2_phonebook/res/app_colors.dart';
+
+class PersonVWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return PersonVState();
+  }
+}
+
+class PersonVState extends State<PersonVWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 6),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(24),
+              child: Container(
+                  width: 104,
+                  height: 104,
+                  child: Image.network(
+                      'https://html5css.ru/howto/img_avatar2.png'))),
+        ),
+        SizedBox(
+          height: 9,
+        ),
+        Text(
+          'Jame Michael',
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+        ),
+        SizedBox(
+          height: 6,
+        ),
+        Text(
+          '12 Mins ago',
+          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+        )
+      ],
+    );
+  }
+}
